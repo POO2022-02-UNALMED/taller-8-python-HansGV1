deb7e60c413d0c4094130d4e9e82c3d36e089b45
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-11-15 17:37:00 -0500

    Up

--- deportista.py
+++ deportista.py
@@ -1,17 +1,14 @@
-class Deportista:
+class Deportista():
     def __init__(self, deporte, añosPracticando):
-        if isinstance(deporte, str) and isinstance(añosPracticando, int):
-            self._deporte = deporte
-            self._añosPracticando = añosPracticando
-    
+        self._deporte = deporte
+        self._añosPracticando = añosPracticando
+
     def getDeporte(self):
         return self._deporte
     def setDeporte(self, dep):
-        if isinstance(dep, str):
-            self._deporte = dep
+        self._deporte = dep
         
     def getAñosPracticando(self):
         return self._añosPracticando
     def setAñosPracticando(self, an):
-        if isinstance(an, int):
-            self._añosPracticando = an
+        self._añosPracticando = an
--- futbolista.py
+++ futbolista.py
@@ -1,10 +1,37 @@
-from persona import Persona
 from deportista import Deportista
+from persona import Persona
 
 class Futbolista(Persona, Deportista):
-    listaFutbolistas = []
-    def __init__(self, golesMarcados, tarjetasRojas, piernaHabil):
-        super.__init__()
+    _listaFutbolistas=[]
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+        Persona.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, "Futbol", añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
+        self._listaFutbolistas.append(self)
+
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    def setGolesMarcados(self, gols):
+        self._golesMarcados = gols
+        
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    def setTarjetasRojas(self, ts):
+        self._tarjetasRojas = ts
+        
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    def setPiernaHabil(self, ph):
+        self._piernaHabil = ph
+        
+    @classmethod
+    def getListaFutbolistas(cls):
+        return cls._listaFutbolistas  
+    @classmethod
+    def setListaFutbolistas(cls, lis):
+        cls._listaFutbolistas = lis
+
+    def __str__(self):
+        return "Mi nombre es "+self._nombre+" soy profesional en el deporte "+self._deporte+" Tengo "+str(self._edad)+" años de edad y llevo "+str(self._añosPracticando)+" años en el deporte"
